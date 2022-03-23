6c0f08ab65cb9bde17e28d7fb448b307ad2c8bfd
Jyo561 <jyotiradityakuanar34@gmail.com>
2022-03-23 20:08:57 +0530

    updates

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,11 +1,48 @@
 <template>
 	<div class="banner">
-		<img src="../img/artist.jpg" class="bannerImg" alt="">
+		<img src="../img/artist.jpg" class="bannerImg" alt="" />
+		<div class="content">
+			<div class="breadCrump">
+				<p>
+					Home <span>/Popular Artist</span>
+				</p>
+				<i>
+					<Icon  icon="fe:elipsis-h" />
+				</i>
+			</div>
+			<div class="artist">
+				<div class="left">
+					<div class="name">
+						<h2>A-Ha</h2>
+						<img src="../img/check.png" alt="">
+					</div>
+					<p>
+						<i>
+							<Icon icon="charm:headphones" />
+						</i>
+						11,184,1811 <span>Monthly listeners</span>
+					</p>
+				</div>
+				<div class="right">
+					<a href="#">Play</a>
+					<a href="">
+						<i>
+							<Icon icon="fa-solid:check" />
+						</i>
+						Following
+					</a>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 <script>
+	import { Icon } from '@iconify/vue';
 	export default{
 		name: 'BanNer',
+		components:{
+			Icon
+		}
 	}
 </script>
 <style>
